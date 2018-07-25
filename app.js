36156d2ab1b16d5b81004bbe30b1b73d3659a624
Tare <tare.dgg@gmail.com>
2018-07-25 17:47:11 -0400

    I added functionality to signup. Users can now sign up securely. Next up is to create secure login.

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-// Express and Route Related Imports
+// Express Route, Sessions Related Imports
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var  router = express.Router();
+const app = express();
+
+// Set the template engine to just accept basic html for now
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// use bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// });
+
 
 // DB related Imports
 // var MongoClient = require('mongodb').MongoClient;
@@ -30,40 +48,89 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongoDB connection error:'));
 
 db.once('open', function() {
-  console.log("We did it, we create and connected to a MongoDB Database! ");
+  console.log("We changed something in the database! ");
 });
 
 
-router.get('/', function(req, res){
-  res.render('views/login', function(err, html){
+
+
+
+///express router to handle requests
+app.get('/', function(req, res, next){
+  res.render('login', { signUpConf: false }, function(err, html){
     if (err){
       return next(err);
     }
     res.send(html);
-  }
-}
+  });
+});
 
-router.post('/', function(req, res){
+// UserSchema.statics.authenticate = function( username, password, callback){
+//   User.findOne( {email: email} )
+//     .exec( (err, user) => {
+//       if (err){
+//         return callback(err)
+//       } else if (!user) {
+//         var err = new Error('User not found.');
+//         err.status = 401;
+//         return callback(err);
+//       }
+//       bcrypt.compare(password, user.password, function(err, result){
+//         if (result === true ){
+//           return callback(null, user);
+//         } else {
+//           return callback();
+//         }
+//       });
+//     });
+// };
+
+// app.post('/', function(req, res, next){
+//   if (
+//     req.body.username &&
+//     req.body.password
+//   )
+// }
+
+app.get('/sign_up', function(req, res, next){
+    res.render('sign_up', function(err, html){
+      if (err){
+        return next(err);
+      }
+      res.send(html);
+    });
+});
+
+app.post('/sign_up', function(req, res, next){
+  console.log("Recieved Form Data... Trying to process it. ");
+  console.log("Here is the request body: ");
+
+  console.log(req.body)
+
   if (
     req.body.email &&
     req.body.password &&
     req.body.passwordConf){
       // get the new User's information from the request body
+      console.log("Making a little progress.");
       var userDataToBePostedToDB = {
         email: req.body.email,
         username: req.body.username,
         password: req.body.password,
         passwordConf: req.body.passwordConf,
       }
-
+      console.log("Almost done.");
       User.create(userDataToBePostedToDB, function(err, user){
         if (err) {
           return next(err);
         } else {
-          return res.redirect('/login'); // redirect to login view with some message to confirm that user was create successfully.
+          console.log("Done!");
+          return res.render('login', { signUpConf: true } ); // redirect to login view with some message to confirm that user was create successfully.
         }
       })
 
 
     }
 })
+
+app.listen(3000, () => "Up and running!\nListening for requests.\nYour wish is my command Sensei.");
--- models/user_model.js
+++ models/user_model.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var bcryt = require('bcrypt');
+var bcrypt = require('bcrypt');
 
 
 var UserSchema = new mongoose.Schema({
@@ -29,17 +29,17 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
-UserSchema.pre('save', function(next) ){ // wtf is with these next functions ... I hope this wont become a problem
-  var user = this;
-  bycrpt.hash(user.password, 10, function( err, hash){
-    if (err){
-      return next(err);
-    } else {
-      user.password = hash;
-      next();
-    }
-  })
-}
+// UserSchema.pre('save', function(next){ // wtf is with these next functions ... I hope this wont become a problem
+//   var user = this;
+//   bycrpt.hash(user.password, 10, function( err, hash){
+//     if (err){
+//       return next(err);
+//     } else {
+//       user.password = hash;
+//       next();
+//     }
+//   })
+// });
 
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
